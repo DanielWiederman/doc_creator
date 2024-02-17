07d21d6a662976cf91b36953fc7ccfa0985cf605
Daniel Wiederman <danielwiederman@gmail.com>
2024-02-17 21:26:29 +0200

    fix deploy branch

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/doc_creator/",
+  base: './',
   plugins: [
     react(),
     checker({
